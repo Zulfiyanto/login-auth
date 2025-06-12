e766d544f5a415af71310b8b90b2a5fa95720cac
zulfiyanto <zulfikar@ilcs.co.id>
2025-06-12 17:03:34 +0700

    chore: remove unused files and configurations
    - Deleted package.json, postcss.config.mjs, and prisma/schema.prisma as they are no longer needed.
    - Removed QueryProvider and ReduxProvider components to streamline state management.
    - Deleted SVG files from the public directory that are not in use.
    - Removed authSlice and userSlice along with their respective store configurations to simplify the Redux setup.
    - Cleared tsconfig.json as TypeScript configurations are no longer applicable.

--- app/signin/page.tsx
+++
@@ -1,9 +0,0 @@
-import AuthForm from '../../components/AuthForm'
-
-export default function SignInPage() {
-  return (
-    <div className='min-h-screen flex items-center justify-center'>
-      <AuthForm mode='signin' />
-    </div>
-  )
-}
--- app/signup/page.tsx
+++
@@ -1,9 +0,0 @@
-import AuthForm from '../../components/AuthForm'
-
-export default function SignUpPage() {
-  return (
-    <div className='min-h-screen flex items-center justify-center'>
-      <AuthForm mode='signup' />
-    </div>
-  )
-}
